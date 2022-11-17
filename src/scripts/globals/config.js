f85e4ed4de87ea67704eef8df94b08d173beddba
mochfarrel06 <mochfarrel2606@gmail.com>
2022-11-17 23:05:27 +0700

    Menambahkan folder

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  KEY: 'ef063fcef9bf9381f7c9d66a96c64805',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString,
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+  WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
+};
+
+export default CONFIG;
